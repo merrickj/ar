835a7e5b82b56fdb42ec704e88b88887a48a4ca2
merrickj <jamesmerrick@hotmail.com>
2016-10-21 19:31:11 -0700

    first working round of probability updating with Delavane structure placeholder

--- mvp.py
+++ mvp.py
@@ -15,23 +15,20 @@ import sys
 from scipy.stats import genextreme
 
 class adaptretreat:
-    def __init__(self, regions, prob_flood, init_prob_act, n_iterations):
+    def __init__(self, regions, init_prob_act, n_iterations):
         self.regions = regions
-        self.prob_flood = prob_flood 
         self.init_prob_act = init_prob_act
         self.n_iterations = n_iterations
         self.action = [] # 0 if no action, 1 if action
-        self.hits = []   # Number of times flooded
-        self.potential = 0 # Number of potential flooding events
         self.alpha = min(1,10*(float(1)/self.regions)) # how much weight your own region relative to neighbours
-        self.prob_alpha = []
         self.transition = [0 for x in range(self.regions)]
 
     def populate(self):
         self.action = [0 for x in range(self.regions)]
-        self.prob_alpha = [self.prob_flood for x in range(self.regions)]
+        self.actual_damage = [0 for x in range(self.regions)]
+        self.expected_damage = [100 for x in range(self.regions)]
+        self.total_damage = [0 for x in range(self.regions)]
         self.prob_act = [self.init_prob_act for x in range(self.regions)]
-        self.hits =  [0 for x in range(self.regions)]
         #then we will see who wants to act (assuming in this version that they all should if behaving rationa
         print 'initial:'
         self.plot_eile()    
@@ -43,7 +40,7 @@ class adaptretreat:
     def update_prob_act(self):
         self.calculate_damage()
         for i in range(self.regions):
-            self.prob_act[i] = max(self.init_prob_act,min(1,(self.actual_damage[i]/self.expected_damage[i]))) #for now just assume we only care about our own region
+            self.prob_act[i] = max(self.prob_act[i],self.actual_damage[i]/(self.init_prob_act*self.expected_damage[i])) #for now just assume we only care about our own region
 
     def decide_action(self):
             for i in range(self.regions):
@@ -54,33 +51,40 @@ class adaptretreat:
 
     def calculate_damage(self):
         for i in range(self.regions):
-            s=flood_rv(i)
-            actual_damage[i]=damage(s,i)
+            if self.action[i] == 0:
+                s=self.flood_rv(i)
+                self.actual_damage[i] = self.damage(s,i)
+                self.total_damage[i] = self.total_damage[i] + self.actual_damage[i]
 
-    def flood_rv(region):
-        return genextreme.rvs(c(region),loc(region),scale(region))
 
-    def damage(s,region):
+    def flood_rv(self,region):
+        c=-.5
+        loc=10
+        scale=1
+        return genextreme.rvs(c,loc,scale)
+#        return genextreme.rvs(c(region),loc(region),scale(region))
+
+    def damage(self,s,region):
         #either delavane function calculated from bottom up (more work) or something simpler that I have not worked out yet
+        return s
                                                                         
     def update(self):
         print 'after observation:\t P(act)'
         for iter in range(self.n_iterations):
             self.transition = [0 for x in range(self.regions)]
-            self.is_damaged()
-            self.update_flood_perception(iter)
             self.update_prob_act()
             self.decide_action()
             self.plot_eile()  
             if sum(self.action) == self.regions:
                 print 'ends at observation ',iter
                 print 'alpha this run: ', self.alpha
-                print 'total number of floods due to no action', sum(self.hits)
-                print 'number of floods due to no action by region', (self.hits)
                 break
+        rounded_total_damage=[round(x,0) for x in self.total_damage]
+        print 'Total damage:', rounded_total_damage
 
 
     def plot_eile(self):
+#        print self.actual_damage
         agent_colors = {0:'0', 1:'1'}        
         roundedlist=[round(x,2) for x in self.prob_act]
         for i in range(self.regions):
@@ -93,10 +97,10 @@ class adaptretreat:
 
 
 
-#parameters:(number of regions, prob flood, prob action, number of iterations)
+#parameters:(number of regions, prob action, number of iterations)
 #ar_1 = adaptretreat(10,.2,.5,500)
 arg=sys.argv
-ar_1 = adaptretreat(int(float(arg[1])),float(arg[2]),float(arg[3]),500)
+ar_1 = adaptretreat(int(float(arg[1])),float(arg[2]),500)
 print arg
 ar_1.populate()
 ar_1.update()
