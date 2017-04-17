334d23ece6a5df9a95666b2dbe95505de0b81e5e
merrickj <jamesmerrick@hotmail.com>
2017-04-17 16:50:37 -0700

    cooley flooding cost added

--- gev.py
+++ gev.py
@@ -27,25 +27,25 @@ from scipy.stats import genextreme
 #Ireland8502 0.06265796 0.03573306 0.2064354 
 #Ireland8515 0.06157673 0.03526684 0.2098331
 
-xi=0.2064354
-sigma=0.03573306
-mu=0.06265796
+xi=0.2098331
+sigma=0.03526684
+mu=0.06157673
 
 c = -xi
 loc = mu
 scale = sigma
 
-print 'Ireland8502 flood level (m):'
-print genextreme.rvs(c,loc,scale)/sigma
-print 'mean', genextreme.stats(c,loc,scale,moments='m')
-print 'mean', ((genextreme.stats(c,moments='m')*sigma) + mu)/sigma
+flood=genextreme.rvs(c,loc,scale)/sigma
+#print 'Ireland8515 flood level (m):'
+#print 'actual ',genextreme.rvs(c,loc,scale),'mean:', genextreme.stats(c,loc,scale,moments='m')
+#print 'actual alt', flood , 'mean alt:', ((genextreme.stats(c,moments='m')*sigma) + mu)/sigma
 
 #see note above about dividing by sigma here
 import math
 x=.0923
 gpdf = ((1+(x-mu)/sigma*xi)**(-1/xi))**(xi+1)*math.exp(-1*(1+(x-mu)/sigma*xi)**(-1/xi))/sigma
 gcdf = math.exp(-1*(1+(x-mu)/sigma*xi)**(-1/xi))
-print gpdf, gcdf
+#print 'gpdf: ',gpdf, 'gcdf: ',gcdf
 
 
 
@@ -56,5 +56,47 @@ def integrand(x):
     return x**2
 
 ans,err = quad(integrand,0,1)
-print ans
+#print 'test integral ', ans
 
+#,seg,segid,s1,s10,s100,s1000,smax,longi,lati,slopecst,uplift,area1,area2,area3,area4,area5,area6,area7,area8,area9,area10,area11,area12,area13,area14,area15,area16,length,h0,pop,popdens,wetland,iso,countrylongname,FUNDregion,ypc_scale,cci,gtapland
+
+#56.674,3.96,28351,110.547,0,IRL,Ireland,WEU,1,1.289,0.619785455
+
+a_cooley=[64,6,2,3,1,5.333333333,5.333333333,5.333333333,3,3,3,3,3.25,3.25,3.25,3.25];
+#print a_cooley[15]
+
+#what happened area15?
+def area(y,a):
+    return a[0]*max(0,min(0.5,y))+(a[1]+a[0])/2*max(0,min(1,y-0.5))+a[1]*max(0,min(0.5,y-1.5))+a[2]*max(0,min(1,y-2))+a[3]*max(0,min(1,y-3))+a[4]*max(0,min(1,y-4))+a[5]*max(0,min(1,y-5))+a[6]*max(0,min(1,y-6))+a[7]*max(0,min(1,y-7))+a[8]*max(0,min(1,y-8))+a[9]*max(0,min(1,y-9))+a[10]*max(0,min(1,y-10))+a[11]*max(0,min(1,y-11))+a[12]*max(0,min(1,y-12))+a[13]*max(0,min(1,y-13))+a[14]*max(0,y-14)
+
+#print 'cooley area flooded', area(flood,a_cooley)
+
+
+sigma_k_cooley = 14.43628151582;
+vsl = 9.444821238556;
+rho = 0.518144214230081;
+
+
+
+def psi(e):
+    return e/(1+e);
+
+def damage(e):
+    sigma_k_cooley = 14.43628151582;
+    vsl = 9.444821238556;
+    mu = 0.01;
+    sigma_l_cooley = 110.547;
+    rho = 0.518144214230081;
+    a_cooley=[64,6,2,3,1,5.333333333,5.333333333,5.333333333,3,3,3,3,3.25,3.25,3.25,3.25];
+    return (1-rho)*area(e,a_cooley)*(sigma_k_cooley*psi(e)+sigma_l_cooley*vsl*mu)
+
+
+#print 'damage_snap', damage(flood)
+
+def integrand_cooley(e):
+
+    return damage(e)
+
+ans_c,err_c = quad(integrand_cooley,0,flood)
+#print 'test integral, err_c', ans_c, err_c 
+print 'damage to cooley from flood level ',flood,' is ', ans_c 
